7dea5bdcc29c17743001951e279f009ece67c5da
Marcelo Parisi <parisi@Marcelos-MacBook-Pro.local>
2018-07-18 09:01:31 -0300

    remote repository

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@
 docker rm kubernetes-lb-controller
 docker rmi kubernetes-lb-controller --force
 docker build -t kubernetes-lb-controller .
+
+# Pushing to remote repository
+export MY_REPO="feitnomore"
+docker tag kubernetes-lb-controller:latest $MY_REPO/kubernetes-lb-controller:latest
+docker push $MY_REPO/kubernetes-lb-controller:latest
